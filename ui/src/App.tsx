4676de29b7bd4fdefe5604b153cadbd15388dc01
Max Crosby <mcro0019@student.monash.edu>
2025-04-14 13:56:30 +1000

    up changes

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -6,6 +6,35 @@ const reactLogo = "./assets/react.svg";
 
 export function App() {
   return (
+    <body>
+        <h1 className="text-center align-super stroke-amber-500 text-5xl ">
+            UP PLANNER
+        </h1>
+        <p className="editor-note text-center">
+            This is where I <strong>will</strong> insert some data for user viewing <br/>
+            Here is a list of planned features.
+        </p>
+        <ul>
+          <li>Ability to filter out certain types of transfers from spending total</li>
+          <li>
+              Ability to categorize certain transfers (esp recurring ones) to be included or excluded from spending calculations</li>
+        </ul>
+        <p className="editor-note2 text-center">
+        To find some more information on the connection of up services go to <a href="https://developer.up.com.au/" className="underline">UP</a>.
+        </p>
+        <br/>
+        The api output of the up transaction data will be shown below from the rudimentary javascript file.
+        <br/> <button id="api-button">Retrieve data</button>
+        <br/> <span id="username"></span>
+        <table id="insersion">
+
+        </table>
+      </body>
+  );
+}
+
+export function oldApp() {
+  return (
     <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
       <div className="flex justify-center items-center gap-8 mb-8">
         <img
